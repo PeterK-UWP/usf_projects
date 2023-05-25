bc12e1a5eb7b4efcd162572042a7a940767ee613
PeterK <kveto002@rangers.uwp.edu>
2023-05-25 15:09:10 -0500

    usf t2 graphs

--- tutorials/t1_plot_energy_vs_ionicstep.py
+++ tutorials/t1_plot_energy_vs_ionicstep.py
@@ -22,18 +22,32 @@ def two_column_text_read(file_name):
     return data
 
 
+def energy_change(data):
+    change_list = []
+    for i in np.linspace(0, len(data[1])-1, 7):   # (0, ionic step - 1, ionic step)
+        if i < 6:   # ionic step - 1
+            change = data[1][int(i+1)] - data[1][int(i)]
+            change_list.append(change)
+        if i == 6:  # ionic step
+            continue
+    return change_list
+
+
 def plot_data(data, display_graph):
     x_data = data[0]
-    plt.xlabel('scf step')
+    plt.xlabel('ionic step')
     y_data = data[1]
     plt.ylabel(r'$E$ (eV/atom)')
     plt.plot(x_data, y_data)
     plt.tight_layout()
+
     if display_graph:
         plt.show()
     elif not display_graph:
-        plt.savefig("t0_energy_vs_scf_step.png")
+        plt.savefig("t1_energy_vs_ionic_step.png")
     return
 
-print(two_column_text_read('t0_data'))
-print(plot_data(two_column_text_read('t0_data'), True))  # True for graph, False to save png
+
+print(two_column_text_read('t1_data'))
+print(energy_change(two_column_text_read('t1_data')))
+print(plot_data(two_column_text_read('t1_data'), True))  # True for graph, False to save png
---
+++ tutorials/t2_plot_enthalpy_pressure_ionic_step.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def column_text_read(file_name):
+    try:
+        file = open(file_name)
+    except OSError as error:
+        print(error)
+        return
+
+    content = file.readlines()
+    data = np.zeros([7, (len(content))], float)
+    n = 0
+
+    for line in content:
+        elements = line.split()
+        data[0, n] = float(elements[0])
+        data[1, n] = float(elements[1])
+        data[2, n] = float(elements[2])
+        data[3, n] = float(elements[3])
+        data[4, n] = float(elements[4])
+        data[5, n] = float(elements[5])
+        data[6, n] = float(elements[6])
+        n += 1
+    return data
+
+
+def energy_change(data):
+    change_list = []
+    for i in np.linspace(0, len(data[1])-1, 5):   # (0, ionic step - 1, ionic step)
+        if i < 4:   # ionic step - 1
+            change = data[1][int(i+1)] - data[1][int(i)]
+            change_list.append(change)
+        if i == 4:  # ionic step
+            continue
+    return change_list
+
+
+def enthalpy(data):
+    energy = np.array(data[1])
+    pressure = np.array(data[2])
+    volume = np.array(data[3])
+    enthalpy_array = energy + pressure * volume
+    return enthalpy_array
+
+
+def plot_data(data, enthalpy_data, display_graph):
+    ionic_step_data = data[0]
+    energy_data = data[1]
+    pressure_data = data[2]
+    xx_data = data[4]
+    yy_data = data[5]
+    zz_data = data[6]
+    plot_list = [[energy_data, enthalpy_data, pressure_data], [xx_data, yy_data, zz_data]]
+    figure, axes = plt.subplots(nrows=2, ncols=3, layout='constrained')
+    for row_index, row in enumerate(plot_list):
+        for column_index, column in enumerate(row):
+            axes[row_index][column_index].plot(ionic_step_data, column)
+            axes[row_index][column_index].set_xlabel('Ionic Step')
+            axes[0][0].set_ylabel(r'$E$ (eV/atom)')
+            axes[0][0].set_title('Energy per Ionic Step')
+            axes[0][1].set_ylabel(r'$H$ (eV)')
+            axes[0][1].set_title('Enthalpy per Ionic Step')
+            axes[0][2].set_ylabel(r'$P$ (kB)')
+            axes[0][2].set_title('Pressure/Ionic Step')
+            axes[1][0].set_ylabel(r'$P_{xx}$ (kB)')
+            axes[1][0].set_title('XX Pressure per Ionic Step')
+            axes[1][1].set_ylabel(r'$P_{yy}$ (kB)')
+            axes[1][1].set_title('YY Pressure/Ionic Step')
+            axes[1][2].set_ylabel(r'$P_{zz}$ (kB)')
+            axes[1][2].set_title('ZZ Pressure/Ionic Step')
+    #plt.tight_layout()
+    if display_graph:
+        plt.show()
+    elif not display_graph:
+        plt.savefig("t2_data_vs_ionic_step.png")
+    return
+
+
+#print(column_text_read('t2_data'))
+#print(energy_change(column_text_read('t2_data')))
+print(plot_data(column_text_read('t2_data'), enthalpy(column_text_read('t2_data')), False))  # True for graph, False to save png
