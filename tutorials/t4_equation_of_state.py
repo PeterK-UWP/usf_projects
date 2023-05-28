42e98c1563cfbebc037da1b1a812097752499668
PeterK <kveto002@rangers.uwp.edu>
2023-05-28 15:41:57 -0500

    added t4 for eos

---
+++ tutorials/t4_equation_of_state.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+This code is to plot lattices a, b, c, volume, energy, and enthalpy against pressure,
+as well as plot energy v volume.
+"""
