381c93b7d54eca8d6dfd295d86fd2de1ae3a25d0
PeterK <kveto002@rangers.uwp.edu>
2023-07-31 13:41:41 -0400

    plotting function to call

--- chamaras_tut/timestep_chamara_T1.py
+++ chamaras_tut/timestep_chamara_T1.py
@@ -2,23 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from plotting import data_read, plot_separate_data
 
+# data file names here
 data_files = ["0.5fs.dat", "1.0fs.dat", "2.0fs.dat", "3.0fs.dat", "4.0fs.dat"]
-
+# input data to be processed
 for file in data_files:
     contents = data_read(file)
     plot_separate_data(contents, 0, 1, "$t$ (ps)", "$E$ (eV/atom)", file, 10)
-    # plot running average too
-
+    # running average is plotted too
+    # strip .dat and change . to _
+    name = file.replace('.', '_')
+    name = name.replace('_dat', '')
+    plt.savefig(name)
     plt.show()
 for file in data_files:
     contents = data_read(file)
     # cannot use plot all data, as the x values have separate lengths
     plt.plot(contents[0], contents[1], label=file)
-plt.title("Energy vs Time-step")
-plt.xlabel("$t$ (ps)")
-plt.ylabel("$E$ (eV/atom)")
+plt.title("Energy vs Time-step", fontsize=20)
+plt.xlabel("$t$ (ps)", fontsize=18)
+plt.ylabel("$E$ (eV/atom)", fontsize=18)
 plt.legend()
-plt.show()
+plt.tight_layout()
 plt.savefig("compared_timesteps")
+plt.show()
 
 
--- plotting.py
+++ plotting.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+#print(plt.style.available)
+#plt.style.use('grayscale')
+# '_classic_test_patch', 'bmh', 'classic', 'fast', 'ggplot',
+# 'grayscale', 'seaborn-v0_8-talk', 'seaborn-v0_8-paper', bold axis, bold title,
 def data_read(filename):
     try:
         file = open(filename)
@@ -36,9 +39,10 @@ def plot_all_data(full_data_array, x_index, x_label, y_label, title, labels):
             y_values = full_data_array[i]
             plt.plot(x_values, y_values, label=labels[i])  # this excludes the x_label
             plt.scatter(x_values, y_values)
-            plt.xlabel(x_label)
-            plt.ylabel(y_label)
-            plt.title(title)
+            plt.xlabel(x_label, fontsize=18)
+            plt.ylabel(y_label, fontsize=18)
+            plt.title(title, fontsize=20)
+
             # print(y_values)
         else:
             continue
@@ -56,14 +60,21 @@ def plot_separate_data(full_data_array, x_index_value, y_index_value, x_label, y
         average_y_values.append(np.mean(full_data_array[y_index_value][j:j + windows]))
     for j in range(windows - 1):
         average_y_values.insert(0, np.nan)
-    plt.plot(full_data_array[x_index_value], average_y_values, 'k.-', label='running average')
+
     plt.plot(full_data_array[x_index_value], full_data_array[y_index_value], 'b.-', label=y_label)
+    # color='b', linestyle='-', marker='.',
+
+    plt.plot(full_data_array[x_index_value], average_y_values, 'k.-', label='running average')
+    # color='k', linestyle='-', marker='.', linewidth=3
 
     # plt.scatter(full_data_array[x_index_value], full_data_array[y_index_value])
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(title)
+    plt.xlabel(x_label, fontsize=18)
+    plt.ylabel(y_label, fontsize=18)
+    plt.title(title, fontsize=20)
+
     plt.legend()
+    plt.grid()
+    plt.tight_layout()
     # plt.show()
     # plt.savefig(title)
     return print('specific plot created')
@@ -84,14 +95,12 @@ if __name__ == "__main__":
     """
     print(data_read("y=x^2"))
 
-    #label_array = ['nodes', 'time/node', 't1/tN']
-    #print(plot_all_data(data_read("data.txt"), 0, "no. nodes", "time", "Effectiveness", label_array))
+    # label_array = ['nodes', 'time/node', 't1/tN']
+    # print(plot_all_data(data_read("data.txt"), 0, "no. nodes", "time", "Effectiveness", label_array))
 
-    #print(plot_separate_data(data_read("data.txt"), 0, 1, "nodes", "t1", "time per node"))
+    # print(plot_separate_data(data_read("data.txt"), 0, 1, "nodes", "t1", "time per node"))
     print(plot_separate_data(data_read("y=x^2"), 0, 1, 'x', 'y', 'y=x^2', 5))
-    #print(running_average(data_read("y=x^2"), 5))
-    #label_array = ['x', 'y^2', 'y^3', 'run_avg']
+    # print(running_average(data_read("y=x^2"), 5))
+    # label_array = ['x', 'y^2', 'y^3', 'run_avg']
 
-    #print(plot_all_data(running_average(data_read("y=x^2"), 5), 0, "x", "y", "y=x^2", label_array))
-
-
+    # print(plot_all_data(running_average(data_read("y=x^2"), 5), 0, "x", "y", "y=x^2", label_array))
